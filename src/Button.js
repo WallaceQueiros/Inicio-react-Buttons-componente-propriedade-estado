5e9789b4a3775e8a11c93fdb057000cba843ab4b
WallaceQueiros <wallacequeiros@hotmail.com>
2021-04-21 12:57:04 -0300

    Initial commit

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import{useState} from 'react';
+
+export default function Button(props) {
+    let [counter, setCounter] = useState(1);
+
+    function increment() {
+        setCounter(counter + 1)
+
+    }
+
+    return(
+        <>
+          <span>{counter}</span>
+          <button onClick={increment}>{props.children}</button>
+          <br />
+        </>
+    )
+}
